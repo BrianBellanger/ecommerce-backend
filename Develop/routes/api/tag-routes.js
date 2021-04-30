cb8cbdf49c1a3469fd020ac9780b0e315fe26c7a
Brian Bellanger <brianjbellanger@gmail.com>
2021-04-29 19:58:16 -0500

    Init

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,111 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      // JOIN with products
+      include: [{ model: Product, through: ProductTag}]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      // JOIN with products
+      include: [{ model: Product, through: ProductTag}]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+/* req.body should look like this...
+    {
+      "tag_name": "NEW--TAG",
+    }
+  */
+    Tag.create(req.body)
+    .then((tag) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.productIds.length) {
+        const productTagIdArr = req.body.productIds.map((product_id) => {
+          return {
+            tag_id: tag.id,
+            product_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(tag);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      // {
+      //   "tag_name": "UPDATED TAG",
+      // }
+      tag_name: req.body.tag_name
+    },
+    {
+      // Gets a tag based on the id given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedTag) => {
+      res.json(updatedTag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# ecommerce-backend
+
+# eCommerce-backend
+
+
+
+Table of contents
+=================
+
+<!--ts-->
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Testing](#testing)
+* [License](#license)
+* [Questions](#questions)
+<!--te-->
+
+## Description
+===========
+
+back end for an e-commerce site; Express.js API using Sequelize to interact with a MySQL database.
+
+## Installation
+============
+
+npm i, create and seed database, npm run start
+
+## Usage
+=====
+
+Use Insomnia or other client to make requests to the server
+
+## Contribution
+============ 
+
+TBA
+
+## Testing
+======= 
+
+Testing done end to end
+
+## License
+=======
+
+None
+
+## Questions
+=========
+
+Email:    [brianjbellanger@gmail.com](mailto:brianjbellanger@gmail.com) with additional questions or comments
+
+GitHub:   [github.com/BrianBellanger](https://github.com/BrianBellanger)
